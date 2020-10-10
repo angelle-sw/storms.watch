d009f960b7153164e3b41cdcaa01a5fda045ca13
Zak Angelle <zaknotzak@gmail.com>
2020-10-10 01:10:45 -0500

    Go out of storm mode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -99,6 +99,15 @@
     overflow: hidden;
   }
 
+  .out-of-storm-mode {
+    display: flex;
+    width: 100%;
+    height: calc(100vh - 7em);
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+
   :global(.reddit-title) {
     font-size: 18px;
   }
@@ -141,6 +150,7 @@
   @media (min-width: 1150px) {
     .stream {
       margin: 1em 1em 1em 0;
+      flex: 1 0 calc(50% - 1em);
       flex: 1 0 calc(100% - 1em);
     }
 
@@ -157,209 +167,7 @@
 </svelte:head>
 
 <div class="page">
-  <div class="streams">
-
-    <!-- <div class="stream">
-      <h3>12News</h3>
-      <div class="embed-container">
-        <iframe
-          title="12News"
-          type="text/html"
-          src="https://www.youtube.com/embed/vS9bc5aAqXk?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-    
-    <div class="stream">
-      <h3>KATC</h3>
-      <div class="embed-container">
-        <iframe
-          title="KATC"
-          type="text/html"
-          src="https://www.youtube.com/embed/NLsnRKGmJRg?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div>
-
-    <!-- <div class="stream">
-      <h3>Agenda-Free TV</h3>
-      <div class="embed-container">
-        <iframe
-          title="Agenda-Free TV"
-          type="text/html"
-          src="https://www.youtube.com/embed/Qc4pFwpmKQs?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Reed Timmer</h3>
-      <div class="embed-container">
-        <div
-          class="fb-video"
-          data-href="https://www.facebook.com/reedtimmer2.0/videos/826650501409007/"
-          data-show-text="false"
-          data-width="auto">
-          <blockquote
-
-            cite="https://developers.facebook.com/reedtimmer2.0/videos/826650501409007/"
-            class="fb-xfbml-parse-ignore" />
-        </div>
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Jeff Piotrowski</h3>
-      <div class="embed-container">
-        <iframe
-          title="Jeff Piotrowski"
-          type="text/html"
-          src="https://www.youtube.com/embed/WpEgJ9qysUk?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Delta Field Mission</h3>
-      <div class="embed-container">
-        <iframe
-          title="Delta Field Mission"
-          type="text/html"
-          src="https://www.youtube.com/embed/kFp4ShRIi7I?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <div class="stream">
-      <h3>StormChasingVideo</h3>
-      <div class="embed-container">
-        <iframe
-          title="StormChasingVideo"
-          type="text/html"
-          src="https://www.youtube.com/embed/oznjK8JrwVY?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-
-      <!-- <div class="offline-container">
-        <span class="offline">(currently offline)</span>
-      </div> -->
-    </div>
-
-    <!-- <div class="stream">
-      <h3>Lake Arthur</h3>
-      <div class="embed-container">
-        <iframe
-          src="https://player.twitch.tv/?channel=ohioweathernut&parent=storms.watch&parent=localhost"
-          frameborder="0"
-          allowfullscreen="1" />
-      </div>
-
-      <div class="offline-container">
-        <span class="offline">(currently offline)</span>
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Lake Arthur #2</h3>
-      <div class="embed-container">
-        <iframe
-          title="Lake Arthur #2"
-          type="text/html"
-          src="https://www.youtube.com/embed/HHk3WXd2sUQ?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>svrweather</h3>
-      <div class="embed-container">
-        <iframe
-          title="svrweather"
-          type="text/html"
-          src="https://www.youtube.com/embed/o1GRBm8OtGo?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Lake Charles</h3>
-      <div class="embed-container">
-        <iframe
-          src="https://player.twitch.tv/?channel=mezmerial&parent=storms.watch&parent=localhost"
-          frameborder="0"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Jennings #1</h3>
-      <div class="embed-container">
-        <iframe
-          src="https://player.twitch.tv/?channel=jennifermcmahan&parent=storms.watch&parent=localhost"
-          frameborder="0"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Jennings #2</h3>
-      <div class="embed-container">
-        <iframe
-          src="https://player.twitch.tv/?channel=txwxman1&parent=storms.watch&parent=localhost"
-          frameborder="0"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Lafayette</h3>
-      <div class="embed-container">
-        <iframe
-          src="https://player.twitch.tv/?channel=not_a_bot100&parent=storms.watch&parent=localhost"
-          frameborder="0"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
-    <!-- <div class="stream">
-      <h3>Gulf Coast</h3>
-      <div class="embed-container">
-        <iframe
-          title="Gulf Coast"
-          type="text/html"
-          src="https://www.youtube.com/embed/RPXNIKM5nPQ?autoplay=1&mute=1"
-          frameborder="0"
-          allow="autoplay"
-          allowfullscreen="1" />
-      </div>
-    </div> -->
-
+  <div class="out-of-storm-mode">
+    <h1>We are currently out of storm mode.</h1>
   </div>
-
-  <div class="tropical-twitter">
-    <a
-      class="twitter-timeline"
-      href="https://twitter.com/wx_becks/lists/tropical-twitter?ref_src=twsrc%5Etfw" />
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8">
-
-    </script>
-  </div>
-
-  <iframe class="tropical-reddit" src="./tropical-reddit.html" />
 </div>
