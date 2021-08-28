5e77ff974715920d3fe46489e46e1a8b3b3ec3a7
Zak Angelle <zaknotzak@gmail.com>
2021-08-28 13:39:25 -0500

    add test streams

--- src/pages/StreamsPage.tsx
+++ src/pages/StreamsPage.tsx
@@ -8,27 +8,35 @@ const StreamsPage = () => {
     <ul className="streams">
       <Stream
         title="Stream 1"
-        url="https://www.youtube.com/embed/DFkSBK6s4CI?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/WpEgJ9qysUk?autoplay=1&mute=1"
       />
       <Stream
         title="Stream 2"
-        url="https://www.youtube.com/embed/6D7AmjkeI2o?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/lNCcmAu8Iss?autoplay=1&mute=1"
       />
       <Stream
         title="Stream 3"
-        url="https://www.youtube.com/embed/TZ827lkktYs?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/o1GRBm8OtGo?autoplay=1&mute=1"
       />
       <Stream
         title="Stream 4"
-        url="https://www.youtube.com/embed/961uG4Ixg_Y?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/HHk3WXd2sUQ?autoplay=1&mute=1"
       />
       <Stream
         title="Stream 5"
-        url="https://www.youtube.com/embed/aXqt-10pWpw?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/K_QXbe1k81s?autoplay=1&mute=1"
       />
       <Stream
         title="Stream 6"
-        url="https://www.youtube.com/embed/K_QXbe1k81s?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/kFp4ShRIi7I?autoplay=1&mute=1"
+      />
+      <Stream
+        title="Stream 7"
+        url="https://www.youtube.com/embed/RPXNIKM5nPQ?autoplay=1&mute=1"
+      />
+      <Stream
+        title="Stream 8"
+        url="https://www.youtube.com/embed/e0A4hD2teBc?autoplay=1&mute=1"
       />
     </ul>
   );
