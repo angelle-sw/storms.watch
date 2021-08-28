0951aa698ac714ccf89d5441c39eb5479014bca1
Zak Angelle <zaknotzak@gmail.com>
2021-08-28 15:03:13 -0500

    update streams

--- src/pages/StreamsPage.tsx
+++ src/pages/StreamsPage.tsx
@@ -7,36 +7,36 @@ const StreamsPage = () => {
   return (
     <ul className="streams">
       <Stream
-        title="Stream 1"
-        url="https://www.youtube.com/embed/WpEgJ9qysUk?autoplay=1&mute=1"
+        title="WFAA"
+        url="https://www.youtube.com/embed/yDhRFKNprKE?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 2"
-        url="https://www.youtube.com/embed/lNCcmAu8Iss?autoplay=1&mute=1"
+        title="KHOU I-10/I-12 Traffic Cam"
+        url="https://www.youtube.com/embed/2C8ShyNmhOc?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 3"
-        url="https://www.youtube.com/embed/o1GRBm8OtGo?autoplay=1&mute=1"
+        title="New Orleans, LA"
+        url="https://www.youtube.com/embed/OGjjTASWwYA?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 4"
-        url="https://www.youtube.com/embed/HHk3WXd2sUQ?autoplay=1&mute=1"
+        title="Leeville, LA"
+        url="https://www.youtube.com/embed/Z6OKCfWX5oM?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 5"
-        url="https://www.youtube.com/embed/K_QXbe1k81s?autoplay=1&mute=1"
+        title="New Orleans, LA (French Quarter)"
+        url="https://www.youtube.com/embed/QnWFKx485rA?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 6"
-        url="https://www.youtube.com/embed/kFp4ShRIi7I?autoplay=1&mute=1"
+        title="Biloxi, MS"
+        url="https://www.youtube.com/embed/SIA3m2Ai3hU?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 7"
-        url="https://www.youtube.com/embed/RPXNIKM5nPQ?autoplay=1&mute=1"
+        title="Mobile, AL"
+        url="https://www.youtube.com/embed/a1Cfw_muqJw?autoplay=1&mute=1"
       />
       <Stream
-        title="Stream 8"
-        url="https://www.youtube.com/embed/e0A4hD2teBc?autoplay=1&mute=1"
+        title="Coastal Multicam"
+        url="https://www.youtube.com/embed/WyHud8_dUlA?autoplay=1&mute=1"
       />
     </ul>
   );
