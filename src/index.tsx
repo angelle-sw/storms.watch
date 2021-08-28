5f7114109b07cad763c27d65595de86e4e12083a
JP Angelle <jp@angelle.me>
2021-08-28 02:09:12 -0500

    chore: add admin page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import DashboardRoute from "./Dashboard/DashboardRoute";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <DashboardRoute path="/admin" />
+        <Route path="/" element={<App />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
