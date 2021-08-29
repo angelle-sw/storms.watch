3b94e8625240ccadac2d98a79916e8a0d4f393de
Zak Angelle <zaknotzak@gmail.com>
2021-08-29 10:33:26 -0500

    update streams

--- src/pages/StreamsPage.tsx
+++ src/pages/StreamsPage.tsx
@@ -8,39 +8,51 @@ const StreamsPage = () => {
         url="https://www.youtube.com/embed/yDhRFKNprKE?autoplay=1&mute=1"
       />
       <Stream
-        title="Coastal & Inland Cams"
-        url="https://player.twitch.tv/?channel=hurricams&parent=storms.watch&parent=localhost"
+        title="Southeast LA (Reed Timmer)"
+        url="https://www.youtube.com/embed/LmbE_325rjs?autoplay=1&mute=1"
       />
       <Stream
-        title="Southeast LA"
-        url="https://www.youtube.com/embed/ma03z0MKjxw?autoplay=1&mute=1"
+        title="Morgan City, LA (hurricanetrack)"
+        url="https://www.youtube.com/embed/Abk6KxMAhwI?autoplay=1&mute=1"
+      />
+      <Stream
+        title="Houma, LA (Aaron Jayjack)"
+        url="https://www.youtube.com/embed/wd9jxmceF0Y?autoplay=1&mute=1"
+      />
+      <Stream
+        title="Cocodrie & New Orleans, LA"
+        url="https://www.youtube.com/embed/gGt3OJRLh2c?autoplay=1&mute=1"
       />
       <Stream
         title="New Orleans, LA"
-        url="https://www.youtube.com/embed/OGjjTASWwYA?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/4IxKFLhwlfk?autoplay=1&mute=1"
       />
+      <Stream
+        title="Covington, LA"
+        url="https://player.twitch.tv/?channel=atomis42&parent=storms.watch&parent=localhost"
+      />
+      {/*
+      <Stream
+        title="Coastal & Inland Cams"
+        url="https://player.twitch.tv/?channel=hurricams&parent=storms.watch&parent=localhost"
+      />
+      */}
+      {/*
       <Stream
         title="Leeville, LA"
         url="https://www.youtube.com/embed/Z6OKCfWX5oM?autoplay=1&mute=1"
       />
+      */}
+      {/*
       <Stream
         title="New Orleans, LA (French Quarter)"
-        url="https://www.youtube.com/embed/QnWFKx485rA?autoplay=1&mute=1"
+        url="https://www.youtube.com/embed/1OwvQFnwsyU?autoplay=1&mute=1"
       />
+      */}
       <Stream
         title="Biloxi, MS"
         url="https://www.youtube.com/embed/SIA3m2Ai3hU?autoplay=1&mute=1"
       />
-      <Stream
-        title="Mobile, AL"
-        url="https://www.youtube.com/embed/a1Cfw_muqJw?autoplay=1&mute=1"
-      />
-      {/*
-        <Stream
-          title="Coastal Cams"
-          url="https://www.youtube.com/embed/WyHud8_dUlA?autoplay=1&mute=1"
-        />
-      */}
     </ul>
   );
 };
