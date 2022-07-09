e06ff10b67c31bd209ceee144e5c416996dd1f4c
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-07-08 21:29:10 -0500

    create Header and Logo components and add to App

---
+++ src/Logo.tsx
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <div className="logo-container">
+      <img src="./images/logo.png" className="logo" alt="storms.watch" />
+      <div className="storm-mode-indicator blink"></div>
+    </div>
+  );
+}
+
+export default Logo;
