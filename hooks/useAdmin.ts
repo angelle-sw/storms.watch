9cbb1849b497ef12d881312b8f3dcdc108edfd1b
JP Angelle <jp@angelle.me>
2022-07-11 12:08:18 -0500

    chore: migrate to nextjs apis, fix ci

--- hooks/useAdmin.ts
+++ hooks/useAdmin.ts
@@ -2,20 +2,30 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { parse } from "cookie";
 
-const useAdmin = () => {
+type InitialData = {
+  initialData: {
+    isAdmin: boolean;
+  };
+};
+
+const useAdmin = ({ initialData }: InitialData) => {
   const adminPassphrase =
     (typeof document === "object" && parse(document.cookie)?.adminPassphrase) ||
     "";
 
-  const query = useQuery("getAuth", async () => {
-    const response = await axios.get("/api/getAuth", {
-      headers: {
-        "admin-passphrase": adminPassphrase,
-      },
-    });
+  const query = useQuery(
+    "getAdminStatus",
+    async () => {
+      const response = await axios.get("/api/getAdminStatus", {
+        headers: {
+          "admin-passphrase": adminPassphrase,
+        },
+      });
 
-    return response.data;
-  });
+      return response.data;
+    },
+    { initialData: initialData.isAdmin }
+  );
 
   return query;
 };
--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   styledComponents: true,
-  env: {
-    API_URL: process.env.API_URL,
-  },
 };
 
 module.exports = nextConfig;
